dd0a9149ecde9f4850723ecc2d8646c76998cb92
JDM <jdmartinez1531@gmail.com>
2022-10-21 14:39:33 -0500

    basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Container } from "react-bootstrap";
+import Layout from "./components/Layout";
+
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Construction Shop</h1>
+      <Layout>
+        <main className="py-3">
+          <Container>
+            <h1>Welcome to Construction Shop</h1>
+          </Container>
+        </main>
+      </Layout>
     </div>
   );
 }
